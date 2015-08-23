90f949b503a3b1f873b993f5d9bf7331e7b27b2a
Erik Thorselius <erik.thorselius@gmail.com>
2015-08-23 22:43:57 +0200

    Change to two == in bash script

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ TARGET_GID=$(stat -c "%g" /data)
 EXISTS=$(cat /etc/group | grep ${TARGET_GID} | wc -l)
 
 # Create new group using target GID and add plex user
-if [ $EXISTS = "0" ]; then
+if [ $EXISTS == "0" ]; then
   groupadd --gid ${TARGET_GID} ${GROUP}
 else
   # GID exists, find group name and add
@@ -22,13 +22,13 @@ fi
 usermod -a -G ${GROUP} plex
 
 # Will change all files in directory to be readable by group
-if [ "${CHANGE_DIR_RIGHTS}" = true ]; then
+if [ "${CHANGE_DIR_RIGHTS}" == true ]; then
   chgrp -R ${GROUP} /data
   chmod -R g+rX /data
 fi
 
 # Current defaults to run as root while testing.
-if [ "${RUN_AS_ROOT}" = true ]; then
+if [ "${RUN_AS_ROOT}" == true ]; then
   /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 else
   su plex -c "/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf"
